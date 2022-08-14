cffd1aa7f4b6037a45454726eed351793db4511d
snekha <snekhasuresh211003@gmail.com>
2022-08-14 14:05:00 +0530

    updated avg values in model

--- ML/app.py
+++ ML/app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import pathlib
 data_dir = pathlib.Path("MOCK.csv")
 data1=pd.read_csv(data_dir)
+data1['Average'] = data1.mean(axis=1)
+print(data1)
 model = pickle.load(open('model.pkl','rb'))
 if(model.predict(data1) == 0):
     print("You didn't work enough hours")
